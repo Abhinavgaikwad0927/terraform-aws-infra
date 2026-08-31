0eaf96c957e5a05ac26e758727d99c4825e407ef
Abhinavgaikwad0927 <abhinavgaikwad365@gmail.com>
2026-08-31 16:02:28 +0530

    added updated backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "abhinav-terraform-state-2026"
+    bucket         = "aabhinav-terraform-state-2026"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
