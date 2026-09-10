1696d0ba819ddcea2e25b3bc2853cda3ad63d160
Abhinavgaikwad0927 <abhinavgaikwad365@gmail.com>
2026-09-10 13:22:02 +0530

    added updated  ec2 main.yml

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -8,7 +8,11 @@ iam_instance_profile = var.iam_instance_profile
   vpc_security_group_ids = [
     var.security_group_id
   ]
-
+root_block_device {
+  volume_size = 50
+  volume_type = "gp3"
+  encrypted   = true
+}
   tags = {
     Name = "Terraform-EC2"
   }
